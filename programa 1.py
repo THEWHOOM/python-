f536e499dbc0a4e9b8d1aa1557f3b2a7ad5bebcc
THEWHOOM <91351764+THEWHOOM@users.noreply.github.com>
2021-09-24 21:18:34 -0300

    Add files via upload

---
+++ programa 1.py
@@ -0,0 +1,15 @@
+print("------------------------------------------")
+print("creditos Mendell")
+
+
+varNombre = input("ingrese su Nombre:")
+varApillo = input("ingrese su apellido: ")
+varEdad = input("ingrese su Edad: ")
+varmontosolicitud =input("ingrese su MONTO SOLICITADO: ")
+varSalario = input("ingrese su sueldo Mensual: ")
+
+if varmontosolicitud > varSalario: 
+   print("credito supera su sueldo")
+else:
+    print("CREDITO APROBADO")
+    print("ANDA DE PARRANDA Y DISFRUTO LA VIDA")
